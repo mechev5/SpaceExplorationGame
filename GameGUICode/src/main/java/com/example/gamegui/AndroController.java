d50c2ea990768f7ffcbf3e0172b6712f3bc0764a
Jose Cuellar <jcuell2@uic.edu>
2023-11-03 11:14:20 -0500

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	GameGUICode/src/main/resources/com/example/gamegui/images/Andromeda galaxy.jpg
    #	GameGUICode/src/main/resources/com/example/gamegui/images/Square1.jpg
    #	GameGUICode/src/main/resources/com/example/gamegui/images/Square2.png

--- GameGUICode/src/main/java/com/example/gamegui/AndroController.java
+++ GameGUICode/src/main/java/com/example/gamegui/AndroController.java
@@ -3,20 +3,39 @@ package com.example.gamegui;
 import java.io.IOException;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
+import javafx.event.EventHandler;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.*;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.control.TextArea;
+
+import java.net.URL;
+import java.util.ResourceBundle;
 
 
 
 
 public class AndroController {
 
+    @FXML
+    ImageView inv1, inv2, inv3, inv4, inv5, inv6, inv7, inv8;
     private Stage stage;
     private Scene scene;
     private Parent root;
+
+    @FXML
+    Label locationLabel, fuelLabel, durabilityLabel;
 
     public void switchToScene1(ActionEvent event) throws IOException {
         root = FXMLLoader.load(getClass().getResource("AScene1.fxml"));
@@ -49,4 +68,21 @@ public class AndroController {
         stage.setScene(scene);
         stage.show();
     }
+
+    public void switchToScene5(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("AScene5.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void switchToScene6(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("AScene6.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
 }
