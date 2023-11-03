9e239aa92cf7e9ebe126a42f7f135270854179ca
Jose Cuellar <jcuell2@uic.edu>
2023-11-03 09:30:29 -0500

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	GameGUICode/src/main/resources/com/example/gamegui/images/Andromeda galaxy.jpg
    #	GameGUICode/src/main/resources/com/example/gamegui/images/Square1.jpg
    #	GameGUICode/src/main/resources/com/example/gamegui/images/Square2.png

---
+++ GameGUICode/src/main/java/com/example/gamegui/AndroController.java
@@ -0,0 +1,52 @@
+package com.example.gamegui;
+
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+
+
+public class AndroController {
+
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+    public void switchToScene1(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("AScene1.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void switchToScene2(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("AScene2.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void switchToScene3(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("AScene3.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void switchToScene4(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("AScene4.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+}
